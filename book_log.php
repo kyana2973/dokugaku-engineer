d3e02e40e6b3ee89b2cdf239ebdedf0409306e61
kyana2973 <22klirk22@gmail.com>
2021-12-12 19:37:05 +0900

    読書ログのアプリ作成

--- book_log.php
+++ book_log.php
@@ -63,12 +63,3 @@ while (true) {
         break;
     }
 }
-
-
-
-
-// echo "書籍名：銀河鉄道の夜" . PHP_EOL;
-// echo "著者名：宮沢賢治" . PHP_EOL;
-// echo "読書状況：読書中".PHP_EOL;
-// echo "評価：５".PHP_EOL;
-// echo "感想：とてもよかった".PHP_EOL;
