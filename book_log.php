d3a3da3ea057369c9425dc6e28fbbdd96cb25745
kyana2973 <22klirk22@gmail.com>
2021-12-12 19:05:51 +0900

    独学エンジニア　課題制作アプリ

---
+++ book_log.php
@@ -0,0 +1,74 @@
+<?php
+
+$reviews = [];
+
+function createReview() {
+    echo "読書ログを登録してください" . PHP_EOL;
+    echo "書籍名：";
+    $title = trim(fgets(STDIN));
+
+    echo "著者名：";
+    $author = trim(fgets(STDIN));
+
+    echo "読書状況(未読, 読んでる, 完読)：";
+    $status = trim(fgets(STDIN));
+
+    echo "評価(５点満点中で何点？)：";
+    $score = trim(fgets(STDIN));
+
+    echo "感想：";
+    $summary = trim(fgets(STDIN));
+
+    echo "読書ログを登録しました" . PHP_EOL . PHP_EOL;
+
+    return [
+        "title" => $title,
+        "author" => $author,
+        "status" => $status,
+        "score" => $score,
+        "summary" => $summary
+    ];
+}
+
+function listReviews($reviews) {
+    echo "読書ログを表示します" . PHP_EOL . PHP_EOL;
+
+    foreach ($reviews as $review) {
+        echo "書籍名：" . $review["title"] . PHP_EOL;
+        echo "著者名：" . $review["author"] . PHP_EOL;
+        echo "読書状況：" . $review["status"] . PHP_EOL;
+        echo "評価：" . $review["score"] . PHP_EOL;
+        echo "感想：" . $review["summary"] . PHP_EOL;
+        echo "_____________________" . PHP_EOL . PHP_EOL;
+    }
+}
+
+
+
+while (true) {
+    echo "1. 読書ログを登録" . PHP_EOL;
+    echo "2. 読書ログの表示" . PHP_EOL;
+    echo "9. アプリケーションを終了" . PHP_EOL;
+    echo "番号を選択してください(1, 2, 9)：";
+    $answer = trim(fgets(STDIN));
+
+    if ($answer === "1") {
+        $reviews[] = createReview();
+
+    } elseif ($answer === "2") {
+        listReviews($reviews);
+
+    } elseif ($answer === "9") {
+        echo "またな！";
+        break;
+    }
+}
+
+
+
+
+// echo "書籍名：銀河鉄道の夜" . PHP_EOL;
+// echo "著者名：宮沢賢治" . PHP_EOL;
+// echo "読書状況：読書中".PHP_EOL;
+// echo "評価：５".PHP_EOL;
+// echo "感想：とてもよかった".PHP_EOL;
---
+++ memo.php
@@ -0,0 +1,51 @@
+<?php
+
+$notes = [];
+
+function createNotes() {
+    echo "タイトル：";
+    $title = trim(fgets(STDIN));
+    echo "内容：";
+    $content = trim(fgets(STDIN));
+    echo "メモを追加しました" . PHP_EOL . PHP_EOL;
+    return [
+        "title" => $title,
+        "content" => $content
+    ];
+}
+
+function listNotes($notes) {
+    foreach ($notes as $note) {
+        echo "タイトル" . $note["title"]  . PHP_EOL;
+        echo "内容" . $note["content"] . PHP_EOL;
+        echo "---------------------------" . PHP_EOL . PHP_EOL;
+    }
+}
+
+echo "My Notes".PHP_EOL;
+
+echo "1. メモの追加".PHP_EOL;
+echo "2. メモの表示".PHP_EOL;
+echo "9. アプリ終了".PHP_EOL.PHP_EOL;
+
+
+
+while (true) {
+    echo "番号を選択してください(1, 2, 9)：";
+    $number = trim(fgets(STDIN));
+
+    if ($number === "1") {
+        //メモの追加
+        $notes[] = createNotes();
+
+    } elseif ($number === "2") {
+        //メモの表示
+        listNotes($notes);
+
+    } elseif ($number === "9") {
+        //アプリ終了
+        echo "Bye!!!";
+        break;
+
+    }
+}
